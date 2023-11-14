1c1e760658bd6856611c726c3cd4308f820b9d26
h8008 <hugh18012@gmail.com>
2023-11-14 12:20:06 +0000

    Added more code

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import heapq
 import numpy as np
 
 input = [[0, 7, 9, 0, 0, 14],
-               [7, 0, 10, 15, 0, 0],
-               [9, 10, 0, 11, 0, 2],
-               [0, 15, 11, 0, 6, 0],
-               [0, 0, 0, 6, 0, 9],
-               [14, 0, 2, 0, 9, 0]]
+         [7, 0, 10, 15, 0, 0],
+         [9, 10, 0, 11, 0, 2],
+         [0, 15, 11, 0, 6, 0],
+         [0, 0, 0, 6, 0, 9],
+         [14, 0, 2, 0, 9, 0]]
 
 class Heap:
     def __init__(self, data=[]):
@@ -17,9 +17,9 @@ class Heap:
         node = Node(val)
         heapq.heappush(self.heap, node)
 
-    def pop(self, val):
+    def pop(self):
         top = self.top()
-        heapq.heappop()
+        heapq.heappop(self.heap)
         return top
     
     def top(self):
@@ -32,33 +32,48 @@ class Node:
     def __init__(self, val):
         self.val = val
 
-    def __lt__(self, val):
-        return self.val[2] < val[2]
+    def __lt__(self, node):
+        return self.val[2] < node.val[2]
     
-def initHeap (input):
+def initHeap (input, processed):
     heap = Heap([])
-    for i, row_i in enumerate(input):
-        for j, col_j in enumerate(row_i):
-            w = input[i][j]
-            src = i
-            dest = j
-            heap.push((src, dest, w))
+    for j, col_j in enumerate(input[0]):
+        w = input[0][j]
+        src = 0
+        dest = j
+        heap.push((src, dest, w))
+    processed[0] = True
     return heap
 
 def initShortestPath (input):
     sp = dict()
     for i, row_i in enumerate(input):
         sp[i] = np.inf
-
+    sp[0] = 0
     return sp
 
+def initProcessed (input):
+    return np.full(np.array(input).shape, False)
+
+def getDests (input, src):
+    dests_pos = np.arange(input[src])
+    dests = dests_pos[input[src] != 0]
+    return dests
+    
 def main (input, target):
-    heap = initHeap(input)
+    processed = initProcessed(input)
+    heap = initHeap(input, processed)
     sp = initShortestPath(input)
 
     while(not heap.empty()):
-        src, dest, weight = heap.pop()
-        sp[dest] = min(sp[src] + weight, sp[dest])
+        src, dest, weight = heap.pop().val
+        if (not processed[src][dest]):
+            sp[dest] = min(sp[src] + weight, sp[dest])
+            dests = getDests(input, src)
+            for d in enumerate(dests):
+                heap.push(d)
+            processed[src][dest] = True
+            print(sp[dest])
 
     print(sp[target])
 
